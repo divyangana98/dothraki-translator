396efd0e53824b59df72707064607ed7ef7ce4b0
Divyangana <divyangana97@gmail.com>
2021-09-29 17:27:06 +0530

    Added footer, style and translation url

---
+++ app.js
@@ -0,0 +1,29 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text="+ text;
+}
+
+function errorHandler(error){
+    console.log("Error is "+error);
+    alert("Maximum limit of translation has been reached. Please try again after sometime.");
+}
+
+function clickHandler(){
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json=>{
+            var outputText = json.contents.translated;
+            outputDiv.innerText = outputText;
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click",clickHandler);
